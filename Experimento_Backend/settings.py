fb5a8587082064edc2150e1d2613ed2bbb76577b
fabian-castro-c <fabian.castro.c@ug.uchile.cl>
2024-07-10 19:14:07 -0400

    Allowed Host sebamon

--- Experimento_Backend/settings.py
+++ Experimento_Backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=gh=_h&76tj09gj+&kk=+n&an+43$_vw-a-qvy@hgda4p*ceni
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sebamon.pythonanywhere.com']
 
 
 # Application definition
